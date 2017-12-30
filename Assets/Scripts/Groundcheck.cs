703e71e723a52e81fdcfa5c9d7b200e29e2e6cb8
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-12-30 12:39:29 -0200

    Implementing groundcheck and adding initial Katarina assets

---
+++ Assets/Scripts/Groundcheck.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Groundcheck : MonoBehaviour {
+
+    public Animator targetAnimator;
+    public bool onGround { get; private set; }
+
+    void OnTriggerStay2D(Collider2D other) {
+        onGround = true;
+        SetAnimatorFlag();
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        onGround = false;
+        SetAnimatorFlag();
+    }
+
+    void SetAnimatorFlag() {
+        if (targetAnimator)
+            targetAnimator.SetBool("onGround", onGround);
+    }
+}
