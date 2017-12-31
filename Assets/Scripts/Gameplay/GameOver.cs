5e0f218cc4de698363ca1c1cbe3c0ee7b87c65fe
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-12-31 00:53:52 -0200

    Implementing game over

---
+++ Assets/Scripts/Gameplay/GameOver.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour {
+
+    public GameObject gameOverText;
+
+    bool gameEnded;
+
+    void Update() {
+        if (!gameEnded) {
+            if (Katarina.instance.GetComponent<HitPoints>().Died())
+                gameEnded = true;
+        }
+        else {
+            gameOverText.SetActive(true);
+            if (GameInput.GetAxisDown(InputAxes.jump)) {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+    }
+}
