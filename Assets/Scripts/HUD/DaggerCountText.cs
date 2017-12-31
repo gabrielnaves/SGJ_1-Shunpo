6a0f230526b2660345fd4daffd239320fd95e515
Gabriel Naves da Silva <gabri.navess@gmail.com>
2017-12-30 23:54:43 -0200

    Added dagger count to HUD

---
+++ Assets/Scripts/HUD/DaggerCountText.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DaggerCountText : MonoBehaviour {
+
+    Text text;
+
+    void Awake() {
+        text = GetComponent<Text>();
+    }
+
+    void Update() {
+        text.text = "Daggers: " + DaggerCount.instance.currentDaggers;
+    }
+}
